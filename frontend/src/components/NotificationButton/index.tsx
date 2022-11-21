2836a77d813303f5cdd0f0949b53ac0b75d82264
pedrolima9 <pedro.lima@hexalab.com.br>
2022-11-21 04:13:57 -0300

    send notification with button

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,21 @@
 import icon from "../../assets/img/notification-icon.svg";
-
+import axios from "axios";
+import { BASE_URL } from "../../utils/request";
 import "./styles.css";
 
-export const NotificationButton = () => {
+type Props = {
+  saleId: number;
+};
+
+export const NotificationButton = ({ saleId }: Props) => {
+  function handleClick(id: number) {
+    axios
+      .get(`${BASE_URL}/sales/${id}/notification`)
+      .then(() => alert("Notificação enviada com sucesso!"));
+  }
+
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
       <img src={icon} alt="Ícone notificação" />
     </div>
   );
