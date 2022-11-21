fca4db1e2c0eadfb8303e0055030bcc4fe9e505e
pedrolima9 <pedro.lima@hexalab.com.br>
2022-11-21 01:12:09 -0300

    first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import { NotificationButton } from "./components/NotificationButton";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
+      <NotificationButton />
     </div>
   );
 }
---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,9 @@
+import icon from "../../assets/img/notification-icon.svg";
+
+export const NotificationButton = () => {
+  return (
+    <div className="dsmeta-red-btn">
+      <img src={icon} alt="Ícone notificação" />
+    </div>
+  );
+};
