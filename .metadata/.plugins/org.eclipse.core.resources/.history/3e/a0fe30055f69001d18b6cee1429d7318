0a3b69fea5f1ce2b8b15a71cc93dd18a623a13c1
pedrolima9 <pedro.lima@hexalab.com.br>
2022-11-21 02:49:42 -0300

    Query params at request for get sales for date

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/3e/a0fe30055f69001d18b6cee1429d7318
@@ -0,0 +1,9 @@
+package com.dsmetaph.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dsmetaph.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
--- backend/src/main/java/com/dsmetaph/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/dsmetaph/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.dsmetaph.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.dsmetaph.dsmeta.entities.Sale;
@@ -15,8 +19,15 @@ public class SaleService {
 	@Autowired
 	private SaleRepository respository;
 	
-	public List<Sale> findSales() {
-		return respository.findAll(); 	 
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+
+
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) :  LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today :  LocalDate.parse(maxDate);
+		
+		return respository.findSales(min, max, pageable); 	 
 		
 	}
 }
